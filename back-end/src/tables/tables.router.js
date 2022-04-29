79330f6d24aeddafdcb7d651a685a7cd334e3988
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-04-29 12:15:39 -0400

    fixed table router, controller and service files. Finished backend validation for post /tables route

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const controller = require("./reservations.controller");
+const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
+//
 //router.route("/:table_id/seat")
 
 //router.route("/:table_id")
