f4805200bf3c96e01e002e8e368ec9dc2855b214
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-05-04 15:27:15 -0400

    added the correct order for tables data

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -5,6 +5,7 @@ function list(){
     return knex("tables")
         .leftJoin("reservations", "reservations.table_id", "tables.table_id")
         .select("tables.table_id","tables.table_name","tables.capacity", "reservations.reservation_id as occupied")
+        .orderBy("tables.table_name");
 }
 
 function create(table) {
