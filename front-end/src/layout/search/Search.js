16829eafee2e6d0d189d6027c229a303925e254a
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-05-06 22:53:56 -0400

    frontend functionality finished

--- front-end/src/layout/search/Search.js
+++ front-end/src/layout/search/Search.js
@@ -1,20 +1,66 @@
 import React from "react";
 import ReservationTable from "../../dashboard/ReservationTable";
-import {useEffect, useState} from "react-router-dom"
+import {useEffect, useState} from "react";
+import { listReservations } from "../../utils/api";
+import ErrorAlert from "../ErrorAlert";
 
 function Search(){
 
-    const [formData, setFormData] = useState(initForm)
+    const initForm = {
+        mobile_number: ''
+    }
+    const initReslt = [];
+
+    const [formData, setFormData] = useState(initForm);
+    const [dataToSend, setDataToSend] = useState(null)
+    const [srchReslt, setSrchReslt] = useState(initReslt);
+    const [apiRes, setApiRes] = useState(null)
+    const [apiError, setApiError] = useState(null)
+    const [callApi, setCallApi] = useState(null)
+
+    // captures data from user input
+    const formChangeHandler = ({target}) => {
+        setFormData({
+            ...formData,
+            [target.name]:target.value,
+        });
+    };
+
+     // handles the form submit process
+     const formSubmitHandler = (event) => {
+        event.preventDefault();
+        setDataToSend(formData);
+        setCallApi(true);
+        setFormData(initForm);
+        setApiRes(null)
+    };
+
+    useEffect(()=>{
+        if(callApi) {
+        const abortController = new AbortController();
+        setApiError(null);
+        listReservations(dataToSend, abortController.signal)
+          .then((data)=>{
+              setCallApi(null)
+              setSrchReslt(data)
+              setApiRes(true)
+            })
+          .catch(error=> setApiError([error]));
+        return () => abortController.abort();
+        }
+    },[callApi,dataToSend]);
 
     return(
         <div>
             <h1>Search Reservations</h1>
-            <form>
-            <label htmlFor="mobile_number">Mobile Number</label>
-            <br />
-            <input name="mobile_number" id="mobile_number" type="text" placeholder="Enter a customer's phone number" /><button type="submit">Find</button>
+            {apiError?<ErrorAlert error={apiError} />:null}
+            <form onSubmit={formSubmitHandler}>
+                <label htmlFor="mobile_number">Mobile Number</label>
+                <br />
+                <input name="mobile_number" id="mobile_number" type="text" placeholder="Enter a customer's phone number" value={formData.mobile_number} onChange={formChangeHandler} /><button type="submit">Find</button>
             </form>
-            <ReservationTable />
+           {apiRes && srchReslt.length > 0?<ReservationTable reservations={srchReslt} />:
+           apiRes && srchReslt.length == 0? (<p>No reservations found</p>): null }
         </div>
     )
 };
--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -64,6 +64,7 @@ export async function listReservations(params, signal) {
   Object.entries(params).forEach(([key, value]) =>
     url.searchParams.append(key, value.toString())
   );
+  console.log("from API", url)
   return await fetchJson(url, { headers, signal }, [])
     .then(formatReservationDate)
     .then(formatReservationTime);
