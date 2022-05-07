25b0e6cad738d69c176e8de32b8f4f2b5e5c243c
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-05-06 21:34:53 -0400

    built search comp, added reservationTable comp for search results

--- front-end/src/layout/search/Search.js
+++ front-end/src/layout/search/Search.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import ReservationTable from "../../dashboard/ReservationTable";
+import {useEffect, useState} from "react-router-dom"
 
 function Search(){
 
-    return(<h1>Search Reservations</h1>)
+    const [formData, setFormData] = useState(initForm)
+
+    return(
+        <div>
+            <h1>Search Reservations</h1>
+            <form>
+            <label htmlFor="mobile_number">Mobile Number</label>
+            <br />
+            <input name="mobile_number" id="mobile_number" type="text" placeholder="Enter a customer's phone number" /><button type="submit">Find</button>
+            </form>
+            <ReservationTable />
+        </div>
+    )
 };
 
 
