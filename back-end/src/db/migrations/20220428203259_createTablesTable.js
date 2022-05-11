be0e391b7bc4996233b1ed3f6d1dd02b6009302a
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-05-11 17:45:16 -0400

    fixed migration script causing potentially causing a jest test failure

--- back-end/src/db/migrations/20220428203259_createTablesTable.js
+++ back-end/src/db/migrations/20220428203259_createTablesTable.js
@@ -11,5 +11,8 @@ exports.up = function(knex) {
 
 // Drop the table's table on roll back. 
 exports.down = function(knex) {
-    return knex.schema.dropTable("tables");
+
+    return  knex.schema.table("reservations", (table)=>{
+        table.dropColumn("table_id");
+    }).then(()=>knex.schema.dropTable("tables"))
 };
